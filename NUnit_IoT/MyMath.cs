38d8934f29836aa418e29bc8671df2511692317b
raghavrawat98 <49871652+raghavrawat98@users.noreply.github.com>
2022-03-09 00:10:36 +0530

    MyMath basic Assert

---
+++ NUnit_IoT/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_IoT
+{
+    public class MyMath
+    {
+        public int Addi(int a, int b) {
+            return a + b;
+        }
+
+        public int Subi(int a, int b) {
+            return a - b;
+        }
+    }
+}
---
+++ NUnit_IoT/MyTestCase.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_IoT
+{
+    [TestFixture]
+    class MyTestCase
+    {
+        [TestCase]
+        public void Addi()
+        {
+            MyMath mm = new MyMath();
+            int expectedOut = 31;
+            int actualOut = mm.Addi(20,11);
+            Assert.AreEqual(expectedOut,actualOut);
+        }
+
+        [TestCase]
+        public void Subi() {
+            MyMath mm = new MyMath();
+            int expected = 10;
+            int actual = mm.Subi(30,20);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
